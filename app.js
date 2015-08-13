3cbcf207a1f341a7df95a8ceef0a759b383273c3
ryotamac <tennis.suta6@gmail.com>
2015-08-13 22:58:26 +0900

    グローバルつかってしまった”

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ angular.module("App",["ngRoute"])
   $routeProvider
   .when("/", {
     templateUrl: "index-tmpl",
-    controller: "ItunesController"
+    controller: "indexController"
   })
   .when('/folders', {
-    templateUrl: 'folders-tmpl',
-    controller: "ItunesController"
+    templateUrl: 'folderes-tmpl',
+    controller: "folderesController"
   })
+}])
+.service("musicplayer",["$rootScope",function($scope){
+  this.setplayer= function(target){
+    $scope.player_url = target.previewUrl
+    document.getElementById("player").setAttribute("src",target.previewUrl);
+    document.getElementById("player").play();
+    $scope.player_img = target.artworkUrl100
+  }
 }])
 .service("itunes",["$rootScope","$http",function($scope,$http){
   this.search_it = function(keyword){
@@ -37,43 +45,47 @@ angular.module("App",["ngRoute"])
     });
   }
 }])
-.service("folder",["$rootScope",function($scop){
-
-}])
-.controller("ItunesController",["$scope","itunes",function($scope,itunes){
-  $scope.search_itunes = function(keyword){
-    itunes.search_it(keyword);
-  }
-  $scope.setplayer= function(target){
-    $scope.player_url = target.previewUrl
-    document.getElementById("player").setAttribute("src",target.previewUrl);
-    document.getElementById("player").play();
-    $scope.player_img = target.artworkUrl100
-  }
-
-
+.service("folder",["$rootScope",function($scope){
   $scope.folderes=[];
-  $scope.new_folder_mode = false;
-  $scope.newfolder_name = "";
+
   function reset_create_folder(){
     return create_folder = {
       name: "new folder",
-      musics: []
+      tracks: []
     }
-  }
-  $scope.folderes.push(reset_create_folder())
-
-  $scope.new_folder= function(){
+  };
+   this.new_folder= function(){
     reset_create_folder();
-    $scope.new_folder_mode = true;
     console.log("新規フォルダ作成用意")
-  }
-  $scope.add_folder = function(){
-    create_folder.name = $scope.newfolder_name; 
+  };
+  this.add_folder = function(folder_name){
+    create_folder.name = folder_name;
     $scope.folderes.push(create_folder);
     create_folder = null;
-    $scope.new_folder_mode = false;
     console.log($scope.folderes);
+  };
+}])
+.controller("folderesController",["$scope","folder",function($scope,folder){
+  console.log($scope.folderes)
+
+}])
+.controller("indexController",["$scope","itunes","folder","musicplayer",function($scope,itunes,folder,musicplayer){
+  $scope.setplayer = function(target){
+    musicplayer.setplayer(target);
+  }
+  $scope.search_itunes = function(keyword){
+    itunes.search_it(keyword);
+  }
+  $scope.new_folder_mode = false;
+  $scope.newfolder_name = "";
+  $scope.new_folder = function(){
+    folder.new_folder();
+    $scope.new_folder_mode = true;
+  }
+  $scope.add_folder = function(){
+    folder.add_folder($scope.newfolder_name);
+    $scope.new_folder_mode = false;
+    $scope.newfolder_name = null;
   }
   $scope.music_add_folder= function(target){
     $scope.folderes[0].musics.push(target);
@@ -81,13 +93,11 @@ angular.module("App",["ngRoute"])
   $scope.out_new_folder= function(){
     $scope.new_folder_mode = false;
   }
-
   $scope.select_into_track= function(target){
     $scope.target_track = target;
   }
-
   $scope.select_into_folder = function(target_f){
-    target_f.musics.push($scope.target_track);
+    target_f.tracks.push($scope.target_track);
     $scope.target_track = null;
     console.log(target_f)
   }
